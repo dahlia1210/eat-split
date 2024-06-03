4bfc81794374f1155951f49749ff4b608e598d39
dahlia1210 <40342761+dahlia1210@users.noreply.github.com>
2024-06-03 19:27:40 +0330

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,206 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const initialFriends = [
+  {
+    id: 151379,
+    name: "مینا",
+    image: "https://i.pravatar.cc/48?u=151379",
+    balance: -7,
+  },
+  {
+    id: 905342,
+    name: "مریم",
+    image: "https://i.pravatar.cc/48?u=905342",
+    balance: 20,
+  },
+  {
+    id: 478776,
+    name: "زهرا",
+    image: "https://i.pravatar.cc/48?u=478776",
+    balance: 0,
+  },
+];
 
 function App() {
+  const [friends, setFriends] = useState(initialFriends);
+  const [selectedFriend, setSelectedFriend] = useState(null);
+  const [showAddFriendForm, setShowAddFriendForm] = useState(false);
+  function handleShowFriendForm() {
+    setShowAddFriendForm((show) => !show);
+  }
+
+  function handleAddFriend(friend) {
+    setFriends((friends) => [...friends, friend]);
+    setShowAddFriendForm(false);
+  }
+
+  function handleSelection(friend) {
+    setSelectedFriend((cur) => (cur?.id === friend.id ? null : friend));
+    setShowAddFriendForm(false);
+  }
+
+  function handleSplitBill(value) {
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: friend.balance + value }
+          : friend
+      )
+    );
+    setSelectedFriend(null);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <FriendList
+          friends={friends}
+          onSelection={handleSelection}
+          selectedFriend={selectedFriend}
+        />
+        {showAddFriendForm && <FormAddFriend onAddFriend={handleAddFriend} />}
+        <Button onClick={handleShowFriendForm}>
+          {showAddFriendForm ? "بستن" : "اضافه کردن دوست"}
+        </Button>
+      </div>
+      {selectedFriend && (
+        <FormSplitBill
+          onSplitBill={handleSplitBill}
+          selectedFriend={selectedFriend}
+        />
+      )}
     </div>
   );
 }
+function Button({ children, onClick }) {
+  return (
+    <button onClick={onClick} className="button">
+      {children}
+    </button>
+  );
+}
+function FriendList({ friends, onSelection, selectedFriend }) {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend
+          friend={friend}
+          key={friend.id}
+          onSelection={onSelection}
+          selectedFriend={selectedFriend}
+        />
+      ))}
+    </ul>
+  );
+}
+function Friend({ friend, onSelection, selectedFriend }) {
+  const isSelected = selectedFriend?.id === friend.id;
+  return (
+    <li className={isSelected ? "selected" : ""}>
+      <img src={friend.image} alt={friend.image} />
+      <h3>{friend.name}</h3>
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} {friend.balance} تومن به شما بدهکاره.
+        </p>
+      )}
+      {friend.balance < 0 && (
+        <p className="red">
+          شما {Math.abs(friend.balance)} تومن به {friend.name} بدهکاری.
+        </p>
+      )}
+      {friend.balance === 0 && <p>شما و {friend.name} بی‌حساب هستید.</p>}
+      <Button onClick={() => onSelection(friend)}>
+        {isSelected ? "بستن" : "انتخاب"}
+      </Button>
+    </li>
+  );
+}
 
+function FormAddFriend({ onAddFriend }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!name || !image) return;
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+    onAddFriend(newFriend);
+    setName("");
+    setImage("https://i.pravatar.cc/48");
+  }
+  return (
+    <form className="form-add-friend" onSubmit={handleSubmit}>
+      <label>👫 نام </label>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <label>🌄 آواتار</label>
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
+
+      <Button>ثبت</Button>
+    </form>
+  );
+}
+function FormSplitBill({ selectedFriend, onSplitBill }) {
+  const [bill, setBill] = useState(0);
+  const [paidByUser, setPaidByUser] = useState(0);
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
+  const paidByFriend = bill ? bill - paidByUser : "";
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!bill || !paidByUser) return;
+    onSplitBill(whoIsPaying === "user" ? paidByFriend : -paidByUser);
+  }
+  return (
+    <form className="form-split-bill" onSubmit={handleSubmit}>
+      <h2>محاسبه دونگ با {selectedFriend.name}</h2>
+
+      <label>💰 مبلغ کل</label>
+      <input
+        type="number"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
+
+      <label>🧍‍♀️ سهم شما</label>
+      <input
+        type="number"
+        value={paidByUser}
+        onChange={(e) =>
+          setPaidByUser(
+            Number(e.target.value) > bill ? paidByUser : Number(e.target.value)
+          )
+        }
+      />
+
+      <label>👫 سهم {selectedFriend.name}</label>
+      <input type="text" disabled value={paidByFriend} />
+
+      <label>🤑 کی پرداخت می‌کنه؟</label>
+      <select
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
+        <option value="user">شما</option>
+        <option value="friend">{selectedFriend.name}</option>
+      </select>
+
+      <Button>محاسبه دونگ</Button>
+    </form>
+  );
+}
 export default App;
